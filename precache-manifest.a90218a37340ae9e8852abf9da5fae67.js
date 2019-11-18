0e326daaef40581dd0c97f25534019e728fe3717
homura <homura.dev@gmail.com>
2019-11-18 13:22:31 +0800

    Updates

---
+++ precache-manifest.a90218a37340ae9e8852abf9da5fae67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bc2242159664ca8b3e7c3f88d4aca7",
+    "url": "/MoodyBlues/index.html"
+  },
+  {
+    "revision": "8fb9480fadc923cb85a7",
+    "url": "/MoodyBlues/static/css/2.f0730080.chunk.css"
+  },
+  {
+    "revision": "ac42f54d359a5c1080a6",
+    "url": "/MoodyBlues/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "8fb9480fadc923cb85a7",
+    "url": "/MoodyBlues/static/js/2.2f9b5407.chunk.js"
+  },
+  {
+    "revision": "ac42f54d359a5c1080a6",
+    "url": "/MoodyBlues/static/js/main.7154d862.chunk.js"
+  },
+  {
+    "revision": "a0a1bbd444e55a2b33d2",
+    "url": "/MoodyBlues/static/js/runtime-main.85a2d907.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30b4b1e9f339981a5b9a72e992ae7089.js"
+  "/MoodyBlues/precache-manifest.a90218a37340ae9e8852abf9da5fae67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MoodyBlues/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
