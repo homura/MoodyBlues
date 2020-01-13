90593fb4395ba72098d491d8261513e90d3609e6
homura <homura.dev@gmail.com>
2020-01-13 17:39:17 +0800

    refactor: remove unused dependencies

--- src/component/PopEvent.tsx
+++ src/component/PopEvent.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 import { Popover } from 'antd';
 import styled from 'styled-components';
 import { TraceEvent } from '../types';
